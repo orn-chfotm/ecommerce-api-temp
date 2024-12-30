fa504ffe557adad58572551b5fd6212bf9dd7715
orn-chfotm <chfotm@gmail.com>
2024-12-31 05:09:51 +0900

    2024-12-31 Login 인증 구성 _ 인증 토큰 발급

--- src/main/java/com/build/ecommerce/core/jwt/security/login/CustomFormLoginFilter.java
+++ src/main/java/com/build/ecommerce/core/jwt/security/login/CustomFormLoginFilter.java
@@ -1,7 +1,10 @@
 package com.build.ecommerce.core.jwt.security.login;
 
-import com.build.ecommerce.core.jwt.exception.AuthenticationMethodException;
+import com.build.ecommerce.core.jwt.JwtDto;
+import com.build.ecommerce.core.jwt.dto.response.TokenResponse;
+import com.build.ecommerce.core.jwt.exception.AuthenticationFailException;
 import com.build.ecommerce.core.jwt.service.JwtService;
+import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -9,25 +12,45 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 
 import java.io.IOException;
+import java.util.Date;
 
 @Configuration
 public class CustomFormLoginFilter extends AbstractAuthenticationProcessingFilter {
 
+    private final JwtService jwtService;
+
     public CustomFormLoginFilter(AuthenticationManager authenticationManager, JwtService jwtService) {
         super("/v1/login");
         setAuthenticationManager(authenticationManager);
+        this.jwtService = jwtService;
     }
 
     @Override
-    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException, IOException, ServletException {
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws org.springframework.security.core.AuthenticationException, IOException, ServletException {
         if (!request.getMethod().equals(HttpMethod.POST.name())) {
-            throw new AuthenticationMethodException();
+            throw new AuthenticationFailException();
         }
 
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
 
+        if (email.isBlank() || password.isBlank()) {
+            throw new AuthenticationFailException();
+        }
+
+        return getAuthenticationManager().authenticate(
+                CustomFormLoginToken.toUnAuthenticate(email, password)
+        );
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        String email = (String) authResult.getPrincipal();
+        TokenResponse tokenResponse = jwtService.createToken(new JwtDto(email, new Date()));
+        response.setHeader("access-token", tokenResponse.accessToken());
+        response.setHeader("refresh-token" ,tokenResponse.refreshToken());
     }
 }
---
+++ src/main/java/com/build/ecommerce/core/jwt/security/login/CustomUserDetailService.java
@@ -0,0 +1,32 @@
+package com.build.ecommerce.core.jwt.security.login;
+
+import com.build.ecommerce.domain.user.entity.User;
+import com.build.ecommerce.domain.user.exception.UserNotFountException;
+import com.build.ecommerce.domain.user.repository.UserRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Set;
+
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username)
+                .orElseThrow(() -> new UserNotFountException());
+
+        return new CustomUserDetails(
+                user.getEmail(),
+                user.getPassword(),
+                Set.of(new SimpleGrantedAuthority("ROLE_USER"))
+        );
+    }
+}
