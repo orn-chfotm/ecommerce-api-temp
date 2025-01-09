531268f9343c0b1271129534a512fb1260e40ac2
orn-chfotm <chfotm@gmail.com>
2025-01-09 14:02:02 +0900

    2025-01-09 Api 명세 상세 등록

--- src/main/java/com/build/ecommerce/domain/product/controller/ProductController.java
+++ src/main/java/com/build/ecommerce/domain/product/controller/ProductController.java
@@ -5,10 +5,13 @@ import com.build.ecommerce.domain.product.dto.request.ProductRequest;
 import com.build.ecommerce.domain.product.dto.request.ProductSerchRequest;
 import com.build.ecommerce.domain.product.dto.response.ProductResponse;
 import com.build.ecommerce.domain.product.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,6 +19,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/v1/product")
+@RequiredArgsConstructor
 @Tag(name = "제품", description = "제품 관련 Api")
 @ApiResponse(
         responseCode = "200",
@@ -28,16 +32,14 @@ public class ProductController {
 
     private final ProductService productService;
 
-    public ProductController(ProductService productService) {
-        this.productService = productService;
-    }
-
     @PostMapping
+    @Operation(method = "POST", summary = "Insert Prodcut", description = "제품을 등록합니다.")
     public ResponseEntity<SuccessResponse<ProductResponse>> registerProduct(@Valid @RequestBody ProductRequest productRequest) {
         return SuccessResponse.toResponse(productService.insertProduct(productRequest));
     }
 
     @GetMapping
+    @Operation(method = "GET", summary = "Select Prodcut Infomation", description = "제품 정보를 검색합니다.")
     public ResponseEntity<SuccessResponse<List<ProductResponse>>> getProductList(@Valid @RequestBody ProductSerchRequest productSerchRequest) {
         return SuccessResponse.toResponse(productService.findProducts(productSerchRequest));
     }
